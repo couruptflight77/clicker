5907932fd8a3ff0077561d04a9070c92ac30bb79
couruptflight77 <brockpoulson77@gmail.com>
2020-03-14 15:01:12 +1100

    got linux click working

--- Manjaro.js
+++ Manjaro.js
@@ -1,3 +1,17 @@
+var code = 0
+
 function CookieUrl() {
     location.assign("https://orteil.dashnet.org/cookieclicker/")
+}
+
+function LinuxClick() {
+    code = code + 1
+    codeReset()
+}
+
+var codeDiv = document.querySelector("#codeCount");
+
+function codeReset() {
+    codeDiv.innerHTML = "<br><em>" + code + "</em>";
+    
 }
