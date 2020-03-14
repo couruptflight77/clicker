441386a17acda7eda16b4bf3d4a51205af757220
couruptflight77 <brockpoulson77@gmail.com>
2020-03-14 14:24:55 +1100

    Fixed cookie clicker link

--- Manjaro.js
+++ Manjaro.js
@@ -1,3 +1,3 @@
-function cookie() {
-    window.location.replace("https://orteil.dashnet.org/cookieclicker/")
+function CookieUrl() {
+    location.assign("https://orteil.dashnet.org/cookieclicker/")
 }
