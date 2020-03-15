f37e3f15ad24b22e64153eafa63a5776cd421c77
couruptflight77 <brockpoulson77@gmail.com>
2020-03-15 15:23:18 +1100

    fixed number string, reset button

--- Manjaro.js
+++ Manjaro.js
@@ -1,17 +1,20 @@
+var codeCountActive = 1
+
 function codeActivate(){
     setInterval(() => {
         codeReset()
     }, 100);
 }
 
+var code
 var codeLocal
 var playedBefore = localStorage.getItem("code")
 if (playedBefore >= 0) {
-    var code = localStorage.getItem("code")
+    var codeString = localStorage.getItem("code")
+    code = parseInt(codeString)
 } else {
     code = 0
 }
-
 
 function CookieUrl() {
     location.assign("https://orteil.dashnet.org/cookieclicker/")
@@ -24,8 +27,20 @@ function LinuxClick() {
 var codeDiv = document.querySelector("#codeCount");
 
 function codeReset() {
-    codeLocal = code
+    if (codeCountActive === 1) {
     localStorage.removeItem("code")
-    localStorage.setItem("code", codeLocal)
+    localStorage.setItem("code", code)
     codeDiv.innerHTML = "<br><em>" + code + "</em>";
+    }
+}
+
+function Reset() {
+    var youSure = prompt("confirm with y")
+    if (youSure === "y") {
+        codeCountActive = 0
+        setTimeout(100)
+        localStorage.clear()
+        localStorage.setItem("code", 0)
+        location.reload()
+    }
 }
