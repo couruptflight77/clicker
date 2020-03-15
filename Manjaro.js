d27aaa11af6621b084470ab6363a8ac2f1dfb1f0
couruptflight77 <brockpoulson77@gmail.com>
2020-03-15 13:55:43 +1100

    fixed spam click error

--- Manjaro.js
+++ Manjaro.js
@@ -1,4 +1,17 @@
-var code = 0
+function codeActivate(){
+    setInterval(() => {
+        codeReset()
+    }, 100);
+}
+
+var codeLocal
+var playedBefore = localStorage.getItem("code")
+if (playedBefore >= 0) {
+    var code = localStorage.getItem("code")
+} else {
+    code = 0
+}
+
 
 function CookieUrl() {
     location.assign("https://orteil.dashnet.org/cookieclicker/")
@@ -6,12 +19,13 @@ function CookieUrl() {
 
 function LinuxClick() {
     code = code + 1
-    codeReset()
 }
 
 var codeDiv = document.querySelector("#codeCount");
 
 function codeReset() {
+    codeLocal = code
+    localStorage.removeItem("code")
+    localStorage.setItem("code", codeLocal)
     codeDiv.innerHTML = "<br><em>" + code + "</em>";
-    
 }
