b612711fcf658113034e393a2ae4a9c34b435cc6
couruptflight77 <brockpoulson77@gmail.com>
2020-03-20 12:42:00 +1100

    Started OS's

--- Manjaro.js
+++ Manjaro.js
@@ -1,34 +1,47 @@
 var codeCountActive = 1
+var clickCode
 
-function codeActivate(){
-    setInterval(() => {
-        codeReset()
-    }, 100);
-}
-
-var code
-var codeLocal
 var playedBefore = localStorage.getItem("code")
 if (playedBefore >= 0) {
     var codeString = localStorage.getItem("code")
     code = parseInt(codeString)
 } else {
     code = 0
+    localStorage.setItem("linuxClickUpgraded", "0")
 }
 
+function codeActivate(){
+    setInterval(() => {
+        codeReset()
+        }, 1);
+}
+ 
 function CookieUrl() {
     location.assign("https://orteil.dashnet.org/cookieclicker/")
 }
 
 function LinuxClick() {
-    code = code + 1
+    var clickUpgrade = localStorage.getItem("linuxClickUpgraded")
+    if (clickUpgrade >= 0) {
+        clickCode = 1
+    } else {
+        if (clickUpgrade >= 1) {
+            clickCode = clickCode * 2
+        } else {
+            
+        }
+    }
+    code = code + clickCode
 }
 
 var codeDiv = document.querySelector("#codeCount");
+var costUbuntu = document.querySelector('#ucost')
+var costDebian = document.querySelector('#dcost')
+
 
 function codeReset() {
     if (codeCountActive === 1) {
-    localStorage.removeItem("code")
+            localStorage.removeItem("code")
     localStorage.setItem("code", code)
     codeDiv.innerHTML = "<br><em>" + code + "</em>";
     }
@@ -41,6 +54,7 @@ function Reset() {
         setTimeout(100)
         localStorage.clear()
         localStorage.setItem("code", 0)
+        localStorage.setItem("linuxClickUpgraded", 0)
         location.reload()
     }
 }
